017619f7c94ab153c5d6144f3ff5bfb5bdcea9fc
ken <ken.shirou12341234@gmail.com>
2026-02-08 20:19:36 +0900

    melody

--- jazz_guitarist_paper/urls.py
+++ jazz_guitarist_paper/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')), # 追加
+  
     path('', include('tribute.urls'))
 ]
